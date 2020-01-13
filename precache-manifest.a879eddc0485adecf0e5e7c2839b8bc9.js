05f1d074216fb3f485e52c12e3e7b769bb119c27
camille-s <camille.seaberry@gmail.com>
2020-01-13 18:15:58 -0500

    Updates

---
+++ precache-manifest.a879eddc0485adecf0e5e7c2839b8bc9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747355f0d05e915c1320353f118315c8",
+    "url": "/neighborhoods18/index.html"
+  },
+  {
+    "revision": "6c877276a15c4e2f7efe",
+    "url": "/neighborhoods18/static/css/2.fd8d3d10.chunk.css"
+  },
+  {
+    "revision": "ee61131bdfeac61a531a",
+    "url": "/neighborhoods18/static/css/main.6a1da75d.chunk.css"
+  },
+  {
+    "revision": "6c877276a15c4e2f7efe",
+    "url": "/neighborhoods18/static/js/2.a682876b.chunk.js"
+  },
+  {
+    "revision": "00f82d403d5c841fc6665c8c115487b7",
+    "url": "/neighborhoods18/static/js/2.a682876b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee61131bdfeac61a531a",
+    "url": "/neighborhoods18/static/js/main.9c34695b.chunk.js"
+  },
+  {
+    "revision": "62a37c014a4943d2f2a1",
+    "url": "/neighborhoods18/static/js/runtime-main.87d053c8.js"
+  },
+  {
+    "revision": "9144a5106068c9eb823acc9943a2d4a5",
+    "url": "/neighborhoods18/static/media/logo.9144a510.png"
+  }
+]);
