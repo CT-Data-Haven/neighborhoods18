c71e953963ec0b294e08a42a965eb8656b803f64
camille-s <camille.seaberry@gmail.com>
2020-01-24 17:01:20 -0500

    Add download links

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { cityDisplay } from './utils.js';
 
 const Download = (props) => (
   <div className='Download'>
-    Download data <a href={ props.dl }>here</a> (right-click and 'Save as...'), filter and analyze data online on <a href={ props.dw }>data.world</a> (requires free sign-up), or download/clone from <a href={ props.gh }>GitHub</a> (advanced users).
+    {`Download ${ cityDisplay[props.city] } neighborhood data`} <a href={ props.dl }>here</a>, filter and analyze data online on <a href={ props.dw }>data.world</a> (requires free sign-up), or download/clone from <a href={ props.gh }>GitHub</a> (advanced users).
   </div>
 );
 
