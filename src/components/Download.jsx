0333f173934ab49aedac79c0f4aae465fad55561
camille-s <camille.seaberry@gmail.com>
2020-12-15 12:36:17 -0500

    Fix download links--need target=_blank

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -4,7 +4,7 @@ import { cityDisplay } from './utils.js';
 
 const Download = (props) => (
   <div className='Download'>
-    <p><a href={ props.dl }>Download </a>{`${ cityDisplay[props.city] }`} neighborhood data (.csv file), filter and analyze data online on <a href={ props.dw }>data.world</a> (requires free sign-up), or download/clone from <a href={ props.gh }>GitHub</a> (advanced users).</p>
+    <p><a href={ props.dl }>Download </a>{`${ cityDisplay[props.city] }`} neighborhood data (.csv file), filter and analyze data online on <a href={ props.dw } target="_blank" rel="noopener">data.world</a> (requires free sign-up), or download/clone from <a href={ props.gh } target="_blank" rel="noopener">GitHub</a> (advanced users).</p>
 
     <p>{ `${ props.nhood }` } data also available as a formatted, printable profile (PDF) -- <Button variant='info' onClick={ props.onClick }>download</Button></p>
   </div>
